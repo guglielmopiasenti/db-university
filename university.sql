f5d96503663ccd691ee117226f385ea321125986
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-28 17:58:56 +0200

    sixth

--- university.sql
+++ university.sql
@@ -17,7 +17,7 @@ SELECT * FROM `courses` WHERE year = 1 AND `period` = 'I semestre';
 SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND TIME(`hour`) > '14:00:00';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-
+SELECT * FROM `degrees` WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 

be765a9335a13a9d08ba24e6db4eb986b276cc80
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-28 17:51:15 +0200

    fourth

--- university.sql
+++ university.sql
@@ -11,7 +11,7 @@ SELECT * FROM `courses` WHERE `credits` > 10;
 SELECT * FROM `students` WHERE `date_of_birth` >= '1993-01-01';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-
+SELECT * FROM `courses` WHERE year = 1 AND `period` = 'I semestre';
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
